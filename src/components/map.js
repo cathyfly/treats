69b6658cac0ab617d704370a330b81b16c577951
“[firstname <cathyflynnmedia@gmail.com>
2021-08-07 18:31:28 +0100

    map now loading needs markers added

--- src/components/map.js
+++ src/components/map.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import { GoogleMap } from 'react-google-maps';
+import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
+import withScriptjs from 'react-google-maps/lib/withScriptjs';
 
-const map = () => {
+function Map() {
     return (
         <div>
-            
+            <GoogleMap
+            defaultZoom={10}
+                defaultCenter={{ lat: 53.3242381, lng: -6.3857869 }}
+            />
         </div>
-    )
+    );
 }
 
-export default map
+const WrappedMap= withScriptjs(withGoogleMap(Map));
+
+export default function MapMarkers() {
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <WrappedMap
+                googleMapURL={'https://maps.googleapis.com/maps/api/js?kv=3.exp&libraries=geometry,drawing,places&key=AIzaSyAnT2N2TS-szJ61WNPwaxHz6FP0ZyNu2v8'
+                    }
+                loadingElement={<div style={{ height: '100%' }} />}
+                containerElement={<div style={{ height: '100%' }} />}
+                mapElement={<div style={{ height: '100%' }} />}
+            />
+        </div>
+    );
+}
+
